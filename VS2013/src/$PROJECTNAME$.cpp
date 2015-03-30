34558451023a14398e552d18cd11534ddf542769
Simon Kirsten <thesiki24@users.noreply.github.com>
2015-03-30 23:51:38 +0200

    added sample code line

--- VS2013/src/$PROJECTNAME$.cpp
+++ VS2013/src/$PROJECTNAME$.cpp
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
-
+#include <iostream>
 
 int main(int argc, char* argv[]) {
+	std::cout << "Welcome to $PROJECTNAME$" << std::endl;
 
 	return 0;
 }
